63fbe3a03a5e167fbf8a75a044cb47c0dbfae29a
shota akasaka <akasaka180@gmail.com>
2017-08-26 21:48:48 +0900

    add comment

--- ikyu.rb
+++ ikyu.rb
@@ -1,8 +1,15 @@
 #coding: utf-8
 require 'selenium-webdriver'
  
+puts "selenium start"
 driver = Selenium::WebDriver.for :firefox
+puts "selenium end"
 
+puts "go to ikyu.com"
 driver.navigate.to "https://www.ikyu.com"
+
+puts "save screenshot"
 driver.save_screenshot("ikyu.png")
+
+puts "script end"
 driver.quit
