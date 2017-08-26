29b72311088be1bc13976c8396756664735278bb
shota akasaka <akasaka180@gmail.com>
2017-08-26 22:12:44 +0900

    [comment] selenium

--- ikyu.rb
+++ ikyu.rb
@@ -2,14 +2,14 @@
 require 'selenium-webdriver'
  
 puts "selenium start"
-driver = Selenium::WebDriver.for :firefox
+# driver = Selenium::WebDriver.for :firefox
 puts "selenium end"
 
 puts "go to ikyu.com"
-driver.navigate.to "https://www.ikyu.com"
+# driver.navigate.to "https://www.ikyu.com"
 
 puts "save screenshot"
-driver.save_screenshot("ikyu.png")
+# driver.save_screenshot("ikyu.png")
 
 puts "script end"
-driver.quit
+# driver.quit
